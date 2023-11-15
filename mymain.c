4cb02caaceddafad8c535d2f010e7678771d36aa
Alaa Ahmed <alaaahmed16112000@gmail.com>
2023-11-15 10:15:46 -0800

    edit

--- mymain.c
+++ mymain.c
@@ -17,14 +17,13 @@ int main(void)
         arg = _strtok(buffer);
         if (strcmp(arg[0], "exit") == 0)
         {
-            exit(0);
+            exit(EXIT_SUCCESS);
         }
         path = get_path(arg[0]);
         if (path == NULL)
             perror("Farouk&&Badawii: command not found");
 
        	exe_cmd(path, arg);
-
 	free(buffer);
     }
     return (0);
