16462dac045a089cc217aba17e117701181e00e6
FaroukBinLhouita <133813472+FaroukBinLhouita@users.noreply.github.com>
2023-11-15 20:20:55 +0200

    Update exe.c

--- exe.c
+++ exe.c
@@ -25,7 +25,19 @@ int exe_cmd(char *patth, char **argv)
     }
     else
     {
-        wait();
+        wait(&status);
+        if (WIFEXITED(status))
+        {
+            exit_code = WEXITSTATUS(status);
+        }
+        else if (WIFSIGNALED(status))
+        {
+            exit_code = WTERMSIG(status);
+        }
+        else if (WIFSTOPPED(status))
+        {
+            exit_code = WSTOPSIG(status);
+        }
     } 
 
     return (exit_code);
