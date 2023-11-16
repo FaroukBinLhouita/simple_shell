cc93b202fd9ad15823404e0862104a8df717b3ef
FaroukBinLhouita <133813472+FaroukBinLhouita@users.noreply.github.com>
2023-11-16 04:46:45 +0200

    Update exe_env.c

--- exe_env.c
+++ exe_env.c
@@ -3,12 +3,12 @@
  */
 void exe_env(char *env)
 {
-	extern char **environ;
-	int i = 0;
-	while(environ[i])
-	{
-		env = environ[i];
-		printf("%s\n", env);
-		i++;
-	}
+extern char **environ;
+int i = 0;
+while(environ[i])
+{
+env = environ[i];
+printf("%s\n", env);
+i++;
+}
 }
