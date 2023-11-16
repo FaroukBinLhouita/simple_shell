7da9d2b06222aa0c5c4f8ec33fe728db1a24603e
FaroukBinLhouita <133813472+FaroukBinLhouita@users.noreply.github.com>
2023-11-16 04:47:23 +0200

    Update cat_cpy.c

--- cat_cpy.c
+++ cat_cpy.c
@@ -11,35 +11,35 @@ char *_strcpy(char *tmp, char *src)
 char *new_dest = tmp;
 while (*src != '\0')
 {
-    *tmp = *src;
-    tmp++;
-    src++;
+*tmp = *src;
+tmp++;
+src++;
 }
 *tmp = '\0';
 return (new_dest);
 }
 
 /**
- * _strcat - make a copy
- * @tmp: char pointer
- * @src: char pointer
- * Return: string
- */
+* _strcat - make a copy
+* @tmp: char pointer
+* @src: char pointer
+* Return: string
+*/
 char *_strcat(char *tmp, char *src)
 {
-    char *result;
+char *result;
 
-    result = tmp;
+result = tmp;
 
-    while (*tmp != '\0')
-            tmp++;
-    while (*src != '\0')
-    {
-           *tmp = *src;
-           tmp++;
-           src++;
-    }
-    *tmp = '\0';
-    
-    return (result);
+while (*tmp != '\0')
+tmp++;
+while (*src != '\0')
+{
+*tmp = *src;
+tmp++;
+src++;
+}
+*tmp = '\0';
+
+return (result);
 }
