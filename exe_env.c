e88930b6d3c083abdcca867a5ed4017f96a2da6c
Alaa Ahmed <alaaahmed16112000@gmail.com>
2023-11-15 18:22:24 -0800

    :)(:

--- exe_env.c
+++ exe_env.c
@@ -5,13 +5,10 @@ void exe_env(char *env)
 {
 	extern char **environ;
 	int i = 0;
-	if (strcmp(env, "env") == 0)
+	while(environ[i])
 	{
-		while(environ[i])
-		{
-			env = environ[i];
-			printf("%s\n", env);
-			i++;
-		}
+		env = environ[i];
+		printf("%s\n", env);
+		i++;
 	}
 }
