e886748da46ad8d91cd7309506d97cbffa6ca840
FaroukBinLhouita <133813472+FaroukBinLhouita@users.noreply.github.com>
2023-11-16 04:45:46 +0200

    Update mymain.c

--- mymain.c
+++ mymain.c
@@ -6,36 +6,36 @@
 
 int main(void)
 {
-	char *buffer, **arg, *path;
-    while (1)
-    {
-        print_prompt();
-        buffer = _getline(buffer);
+char *buffer, **arg, *path;
+while (1)
+{
+print_prompt();
+buffer = _getline(buffer);
 
-        if (buffer[0] == '\n')
-            continue;
+if (buffer[0] == '\n')
+continue;
 
-        arg = _strtok(buffer);
-	if (strcmp(arg[0], "exit") == 0)
-	{
-            printf("Exiting shell....\n");
-	    free(buffer);
-            exit(EXIT_SUCCESS);
-        }
-	if (strcmp(arg[0], "env") == 0)
-	{
-		exe_env(arg[0]);
-		continue;
-	}
-        path = get_path(arg[0]);
-        if (path == NULL)
-	{
-            perror("Farouk&&Badawii: command not found");
-	    freeup(buffer, path, arg);
-	    continue;
-	}
-        exe_cmd(path, arg);
-        freeup(buffer, path, arg);
-    }
-    return (0);
+arg = _strtok(buffer);
+if (strcmp(arg[0], "exit") == 0)
+{
+printf("Exiting shell....\n");
+free(buffer);
+exit(EXIT_SUCCESS);
+}
+if (strcmp(arg[0], "env") == 0)
+{
+exe_env(arg[0]);
+continue;
+}
+path = get_path(arg[0]);
+if (path == NULL)
+{
+perror("Farouk&&Badawii: command not found");
+freeup(buffer, path, arg);
+continue;
+}
+exe_cmd(path, arg);
+freeup(buffer, path, arg);
+}
+return (0);
 }
