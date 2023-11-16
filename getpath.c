c5c8dbe28608259691c7329b9f1f6d8859dac4cf
AlaaBadawii <141270202+AlaaBadawii@users.noreply.github.com>
2023-11-16 02:45:55 +0200

    Update getpath.c

--- getpath.c
+++ getpath.c
@@ -7,13 +7,11 @@ char *get_path(char *command)
 
     delim = ":";
     path_var = getenv("PATH");
-	
+
     if (path_var)
     {
-        path_var_cpy = _strdup(path_var);
+        path_var_cpy = strdup(path_var);
         cmd_len = _strlen(command);
-        if (path_var_cpy != NULL && path_var_cpy[0] != '\0')
-        {
             path_token = strtok(path_var_cpy, delim);
             for (i = 0; path_token != NULL; i++)
             {
@@ -22,12 +20,12 @@ char *get_path(char *command)
                 if (path_of_cmd == NULL)
                 {
                     perror("tsh: memory allocation error");
-                    return NULL;
+                    free(path_var_cpy);
+                    return (NULL);
                 }
-                _strcpy(path_of_cmd, path_token);
-                _strcat(path_of_cmd, "/");
-                _strcat(path_of_cmd, command);
-		_strcat(file_path, "\0");
+                strcpy(path_of_cmd, path_token);
+                strcat(path_of_cmd, "/");
+                strcat(path_of_cmd, command);
                 if (stat(path_of_cmd, &buff) == 0)
                 {
                     free(path_var_cpy);
@@ -38,23 +36,12 @@ char *get_path(char *command)
                     free(path_of_cmd);
                     path_token = strtok(NULL, delim);
                 }
-		free(path_var_cpy);
-		if (stat(command, &buff) == 0)
-		{
-			free(path_var_cpy);
-			return (path_of_cmd);
-		}
-		else
-		{
-			free(path_of_cmd);
-			path_token = strtok(NULL, delim);
-		}
-	    }
-	    free(path_var_cpy);
-	    if (stat(command, &buff) == 0)
-		return (command);
-	    return (NULL);
-    	}
+            }
+            free(path_var_cpy);
+            if (stat(command, &buff) == 0)
+                return (command);
+
+            return (NULL);
     }
     return NULL;
 }
