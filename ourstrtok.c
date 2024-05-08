7e31684eacf6a1831313cb4a6c9386a59b82f73c
AlaaBadawii <alaaahmed16112000@gmail.com>
2024-05-08 10:53:38 +0300

    check

--- ourstrtok.c
+++ ourstrtok.c
@@ -1,4 +1,6 @@
 #include "shell.h"
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * _ourstrtok - the strtok function
@@ -9,21 +11,25 @@
  */
 char *_ourstrtok(const char *str, char deli)
 {
-	int count = strlen(str), i, j = 0;
-	char new[count + 1];
+        int count = strlen(str), i, j = 0;
+        char *new = malloc((count + 1) * sizeof(char)); // Allocate memory for new array
 
-	for (i = 0; i <= count; i++)
-	{
-		if (str[i] == deli || str[i] == '\0')
-		{
-			new[j] = '\0';
-			j = 0;
-		}
-		else
-		{
-			new[j] = str[i];
-			j++;
-		}
-	}
-	return (new);
+        if (new == NULL) {
+            return NULL; // Memory allocation failed
+        }
+
+        for (i = 0; i <= count; i++)
+        {
+                if (str[i] == deli || str[i] == '\0')
+                {
+                        new[j] = '\0';
+                        j = 0;
+                }
+                else
+                {
+                        new[j] = str[i];
+                        j++;
+                }
+        }
+        return (new);
 }
